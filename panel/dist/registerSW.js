3f6d1acc706fee217ec21487d4f8214089258844
ailiksr <ailiksr@lxiv.secondary.edu.pl>
2026-09-11 10:05:39 +0800

    fix(panel): eliminate syntax error in bundle chunk and fix settings tab crash

--- panel/dist/registerSW.js
+++ panel/dist/registerSW.js
@@ -1,1 +1,10 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    for (const r of regs) r.unregister();
+  });
+}
+if ('caches' in window) {
+  caches.keys().then(keys => {
+    for (const k of keys) caches.delete(k);
+  });
+}
